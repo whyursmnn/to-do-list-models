ea0b614e472a55337fbd1b50c8c92822ee1861d2
whyursmnn <whyurusman@gmail.com>
2025-06-25 20:20:32 +0800

    update main

--- backend/App/main.py
+++ backend/App/main.py
@@ -1,10 +1,26 @@
 # backend/app/main.py
+
+import os
+import sys
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+
+sys.path.insert(0, current_dir)
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles # Untuk melayani file statis
-from app.api import api_router
+from fastapi.staticfiles import StaticFiles
+from app.api import api_router 
 from app.core.database import Base, engine
-import os
+
+
 
 app = FastAPI(
     title="To-Do List Perusahaan API",
@@ -19,7 +35,8 @@ origins = [
    
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5173", 
-    "http://localhost:5173", 
+    "http://localhost:5173",
+    "https://nama-unik-frontend.netlify.app", 
 ]
 
 app.add_middleware(
